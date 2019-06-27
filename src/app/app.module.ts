1d7acd7cb8492d53562592ec8bfab4dd93d63508
lgc-code <luisconstante@yahoo.com>
2019-06-26 20:58:52 -0700

    layout imported

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// owl carousel
+
+import { OwlModule } from 'ngx-owl-carousel';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,7 +14,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    OwlModule
   ],
   providers: [],
   bootstrap: [AppComponent]
